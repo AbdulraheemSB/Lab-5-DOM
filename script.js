c0a70929b208f4e105d24ddb456cc3a188751bf8
Abdulraheem Bashaweeh <abashaweeh@stu.kau.edu.sa>
2024-03-23 22:47:27 +0300

    committed

---
+++ script.js
@@ -0,0 +1,43 @@
+let divImageContainer = document.getElementById("photoGallery");
+
+// create and attach an image to the DOM tree
+let imgElem = document.createElement("img");
+imgElem.src = "./public/images/waterfall.jpg";
+imgElem.width = 650;
+imgElem.height = 550;
+divImageContainer.appendChild(imgElem);
+
+
+let delButtton = document.createElement("button");
+delButtton.innerText = "delete";
+divImageContainer.appendChild(delButtton);
+
+delButtton.addEventListener("click", function() {
+imgElem.remove(); 
+this.remove();
+
+});
+
+let zoomInBtn = document.createElement("button")
+zoomInBtn.innerText = " + ";
+let zoomOutBtn = document.createElement("button")
+zoomOutBtn.innerText = " - ";
+
+divImageContainer.appendChild(zoomInBtn);
+divImageContainer.appendChild(zoomOutBtn);
+
+const scaleFactor = 1.5;
+
+zoomInBtn.addEventListener("click", function(){
+
+imgElem.width *= scaleFactor;
+imgElem.height *= scaleFactor;
+
+})
+
+zoomOutBtn.addEventListener("click", function(){
+
+    imgElem.width /= scaleFactor;
+    imgElem.height /= scaleFactor;
+
+})
